a3ef50ba04f6eaf5722ec6925dcb8868e3ebb87a
AmyStas <a.staszczyk@icloud.com>
2024-07-16 13:45:36 +0200

    fix conditions for seeding map, inserting new reservations, get reservations by date

--- haveaseat-api/haveaseat-api/Repositories/ReservationRepository.cs
+++ haveaseat-api/haveaseat-api/Repositories/ReservationRepository.cs
@@ -18,4 +18,31 @@ public class ReservationRepository(DataContext context) : IReservationRepository
         
         return reservationDtos;
     }
+
+    public async Task<List<ReservationDTO>> GetReservationsByDay(DateOnly date)
+    {
+        List<Reservation> reservationsOnGivenDay = await context.Reservations
+            .Include(r => r.Desk)
+            .Include(r => r.User)
+            .Where(r => r.Date == date)
+            .ToListAsync();
+
+        List<ReservationDTO> reservationDtos = reservationsOnGivenDay
+            .Select(r => new ReservationDTO(r)).ToList();
+
+        return reservationDtos;
+    }
+
+    public async Task<AddReservationDTO> InsertReservations(AddReservationDTO reservation)
+    {
+        Reservation entry = new Reservation
+        {
+            Date = reservation.Date,
+            DeskId = reservation.DeskId,
+            UserId = reservation.UserId
+        };
+        await context.Reservations.AddAsync(entry);
+        await context.SaveChangesAsync();
+        return reservation;
+    }
 }
